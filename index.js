da6bde040e0bacca223272f3c322cba415251942
Kushal Dhungana <k.dhungana@jacobs-university.de>
2020-02-07 10:34:00 +0100

    Added JSON formatted output

--- index.js
+++ index.js
@@ -9,18 +9,53 @@ class formField extends HTMLElement{
               'mode': 'open'
     });
     shadow.appendChild(template.content.cloneNode(true))
-    function display(){
-        const form= shadow.getElementById('city').value;
-        alert(form);
-    }
-    }
+    const zipElement = shadow.querySelector('.zip');
+    //console.log(zipElement);
+    zipElement.addEventListener('input', (x) => {
+      const targetValue = x.target.value;
+      if (targetValue == null)
+        return;
+      if (targetValue in table){
+        console.log("FOUND:"+table[targetValue]);
+        //set value of city
+        shadow.querySelector('.city').value=table[targetValue];
+      }
+    })
+  
+
+    const formElement = shadow.querySelector('#add');
+    formElement.addEventListener('submit',(e)=>{
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const data = Array.from(formData.entries()).reduce((memo, pair) => ({
+      ...memo,
+      [pair[0]]: pair[1],
+    }), {});
+    shadow.querySelector('#output').innerHTML = JSON.stringify(data);
+  });
+  //  })
+
+
+
+
+    /*
+    const toJSON= elements=>[].reduce.call(elements,(data,element)=> {
+      data[element.name]= element.value;
+      return data;
+    })
+    */
+
+
+  }
 }
 /*
 function display(){
-    const form= shadow.getElementById('city').value;
+    const form= document.getElementById('city').value;
     alert(form);
+    return form;
 }
 */
+
 window.customElements.define('address-field', formField);
 
 
