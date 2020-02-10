3507c3a2be688e47bc217b386696f35ee73ab924
Kushal Dhungana <k.dhungana@jacobs-university.de>
2020-02-10 11:51:10 +0100

    Simpple modification

--- Readme.md
+++ Readme.md
@@ -11,7 +11,7 @@ Address Widget
 This program creates a custom element called address-form which is reusable and can be imported in other html pages. This is an example of a reusable web components.
 
 ## Additional info
-This program contains address form containing zip, city, address, street, housenumber and country. If you enter the zip code, the corresponding city will be filled(Currently we have static dummy data, so only fills the city corresponding to the zip code that is in the table object). And Country is fixed here i.e Deutschland. There is a button called info. If you click it shows the information of the form in json format.
+This program contains address form containing zip, city, address, street, housenumber and country. If you enter the zip code, the corresponding city will be filled(Currently we have static dummy data, so only fills the city corresponding to the zip code that is in the table object). And Country is fixed here i.e Deutschland. There is a button called info. If you click it shows the information of the form in json format. And there is a testpage file which make the use of address-form custom element to get the widget.
 
 ## Technologies
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let template= document.getElementById('lol');
-
-const table={'28759':'Bremen','97070':'Würzburg','20095':'Hamburg'};
+//Static table for city lookup
+const table={'28759':'Bremen','97070':'Würzburg','20095':'Hamburg','28195':'Bremen','28277':'Bremen','97072':'Würzburg','97074':'Würzburg'};
 
 class formField extends HTMLElement{
   constructor(){
@@ -10,26 +10,27 @@ class formField extends HTMLElement{
     });
     shadow.appendChild(template.content.cloneNode(true))
     const zipElement = shadow.querySelector('.zip');
+
     zipElement.addEventListener('input', (x) => {
       const targetValue = x.target.value;
       if (targetValue == null)
         return;
       if (targetValue in table){
         console.log("FOUND:"+table[targetValue]);
-        //set value of city
+        //Set value of city
         shadow.querySelector('.city').value=table[targetValue];
       }
     })
 
-
     const formElement = shadow.querySelector('#add');
+    //Event listener for the info button
     formElement.addEventListener('submit',(e)=>{
       e.preventDefault();
       const formData = new FormData(e.target);
       const data = Array.from(formData.entries()).reduce((memo, pair) => ({
       ...memo,
       [pair[0]]: pair[1],
-    }), {});
+      }), {});
     shadow.querySelector('#output').innerHTML = JSON.stringify(data);
   });
   }
